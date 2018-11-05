888c81c6533fa9411ca388191ebe063f9fb69bab
CBF104014 <37218079+CBF104014@users.noreply.github.com>
2018-11-05 18:36:37 +0800

    Add files via upload

---
+++ class-4_11_5/ASP_4_3_word.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ASP_4_3_word.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" defaultbutton="Button2">
+        <div>
+			<asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+			<asp:Button ID="Button1" runat="server" Text="測驗去" OnClick="Button1_Click" />
+
+			<br />
+			<asp:Literal ID="Literal1" runat="server"></asp:Literal>
+            <br />
+
+			-------------------<br />
+&nbsp;<br />
+			<asp:Literal ID="Literal2" runat="server"></asp:Literal>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:Button ID="Button2" runat="server" Text="OK" OnClick="Button2_Click" />
+            <br />
+            <asp:Literal ID="Literal3" runat="server"></asp:Literal>
+        </div>
+    </form>
+</body>
+</html>
---
+++ class-4_11_5/ASP_4_3_word.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    Random r = new Random();
+
+    string eng = "name, open, cat, dog, sky, water, computer, book, cellphone";
+    string chi = "姓名, 打開, 貓咪 ,狗 ,天空, 水, 電腦, 書, 手機";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack){
+
+            string[] eng_val = eng.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] chi_val = chi.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < eng_val.Length; i++)
+            {
+                DropDownList1.Items.Add(new ListItem(eng_val[i], chi_val[i]));
+            }
+            Session["eng_val"] = eng_val;
+            Session["chi_val"] = chi_val;
+            Session["r1"] = 0;
+
+        }
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        Literal1.Text += String.Format("{0} -> {1}<br />", DropDownList1.SelectedItem.Text, DropDownList1.SelectedValue);
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {        
+        init();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string[] eng_val = (string[])Session["eng_val"];
+        string[] chi_val = (string[])Session["chi_val"];
+        int r1 = (int)Session["r1"];
+        if (TextBox1.Text.Equals(eng_val[r1])) { Literal3.Text += "correct! <br />"; }
+        else { Literal3.Text += "wrong! <br />"; }
+        init();
+    }
+
+
+    private void init()
+    {
+        string[] eng_val = (string[])Session["eng_val"];
+        string[] chi_val = (string[])Session["chi_val"];
+        int r1 = r.Next(0, chi_val.Length);
+        Session["r1"] = r1;
+        Literal2.Text = chi_val[r1];
+        string s = eng_val[r1][0].ToString();
+        for (int i = 0; i < eng_val[r1].Length - 2; i++)
+        {
+            s += "_";
+        }
+        s += eng_val[r1][s.Length].ToString();
+        TextBox1.Text = s;
+        TextBox1.Focus();
+        TextBox1.Attributes["onfocus"] = "this.select();";
+    }
+}
